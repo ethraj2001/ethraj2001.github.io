599f0939224c843719d2efd5f42a7f16f1fb4e9b
Ethan Elliot Rajkumar <61441923+erdabravest2001@users.noreply.github.com>
2024-04-30 02:24:12 -0700

    added new profile and js

---
+++ job.js
@@ -0,0 +1,55 @@
+// add night button and change logo
+window.onload = function() {
+    const themeImage = document.getElementById('themeToggleImage');
+    if (localStorage.getItem('theme') === 'day') {
+        document.body.classList.add('day-mode');
+        themeImage.src = 'assets/day-icon.png';
+        themeImage.alt = 'Switch to Night Mode';
+    } else {
+        document.body.classList.remove('day-mode');
+        themeImage.src = 'assets/night-icon.png';
+        themeImage.alt = 'Switch to Day Mode';
+    }
+};
+
+document.getElementById('themeToggle').addEventListener('click', function() {
+    const themeImage = document.getElementById('themeToggleImage');
+    if (document.body.classList.contains('day-mode')) {
+        localStorage.setItem('theme', 'night');
+        themeImage.src = 'assets/night-icon.png';
+        themeImage.alt = 'Switch to Day Mode';
+    } else {
+        localStorage.setItem('theme', 'day');
+        themeImage.src = 'assets/day-icon.png';
+        themeImage.alt = 'Switch to Night Mode';
+    }
+});
+
+
+
+// let jobExperiences = [
+//     {   title: "UBC Chemistry Department",
+//         role: "Teaching Assistant for CHEM 1XX Labs/Resource Center",
+//         tag: "Teaching",
+//         duration: {
+//             start: "May 2022",
+//             end: "April 2023"
+//         },
+//         location: "University of British Columbia",
+//         responsibilities: [
+//           " Facilitate weekly lab sections for a diverse group of 24 students, ensuring a supportive and engaging learning environment",
+//           " Conduct regular office hours to address student questions and clarify misunderstandings related to course content, fostering a deeper understanding and academic success", 
+//           " Perform detailed demonstrations to train students on the proper and safe usage of lab equipment, enhancing their technical skills and practical understanding",
+//           " Evaluate and grade student assignments with a focus on providing timely, constructive feedback to encourage academic growth and improvement",
+//           " Maintain a high level of approachability and responsiveness, receiving consistently positive feedback from both students and faculty for contributions to the educational community"
+//         ]        
+//     },
+//     {     title : "Dr. Roman Krems Group",
+//           role: "Teaching Assistant for CHEM 1XX Labs/Resource Center",
+
+//     }
+
+//     { title
+
+//     }
+// ];
