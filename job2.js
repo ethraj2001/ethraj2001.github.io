7ec2c918dda4433e8cbe0e2274c619bd32372b80
Ethan Elliot Rajkumar <61441923+erdabravest2001@users.noreply.github.com>
2024-05-27 14:44:07 -0400

    added new font

--- job2.js
+++ job2.js
@@ -60,10 +60,10 @@ let transTheme = () => {
 //         role: "Teaching Assistant for CHEM 1XX Labs/Resource Center",
 //         tag: "Teaching",
 //         duration: {
-//             start: "May 2022",
+//             start: "September 2021",
 //             end: "April 2023"
 //         },
-//         location: "University of British Columbia",
+//         location: "The University of British Columbia",
 //         responsibilities: [
 //           " Facilitated weekly lab sections for a diverse group of 24 students, ensuring a supportive and engaging learning environment",
 //           " Conducted regular office hours to address student questions and clarify misunderstandings related to course content, fostering a deeper understanding and academic success", 
@@ -73,11 +73,58 @@ let transTheme = () => {
 //         ]        
 //     },
 //     {     title : "Dr. Roman Krems Group",
-//           role: "Teaching Assistant for CHEM 1XX Labs/Resource Center",
+//           role: "NSERC CREATE Undergraduate Research Assistant, Directed Studies Student",
+//           tag: "Research, Internship", 
+//           duration: {
+//               start: "May 2022",
+//               end: "April 2024"
+//           }, 
+//           location: "The University of British Columbia",
+//           responsibilities: [
+//             "Developed and improved group website using HTML, CSS, and JavaScript, enhancing user experience and accessibility of published research", 
+//             "Implemented error-mitigation techniques and recoded existing scripts for hybrid to be run on IBM Quantum Computers (i.e. IBM System One)", 
+//             "Identifying the impact of quantum hardware constraints on use of DVR quantum algorithms", 
+//             "Working on the devlopment of a new quantum algorithm that uses Gaussian Processes to design quantum circuits"
+//           ]
+//      }      
+//     {   title: "UBC BIOMOD", 
+//         role: "Wetlab and Computational Team Lead, Undergraduate Researcher, Developer", 
+//         tag: ["Research","Student Leadership"] 
+//         location: "The University of British Columbia (Life Sciences Institute)",
+//         projects: {
+//           project_1: {
+//             name: "The Nanohinge: Diagnosing the Problem of Complex Structure Design in a Low Carbon Economy", 
+//             responsibilities: [
+//             "Designed (using CaDNAno) and optimized DNA-origami structures (using Python) that self-actuate and can self-assemble with applications in drug-delivery, immunology and nanosurgery", 
+//             "Developed python scripts to locate design flaws in DNA origami structures via JSON coordinate analysis",
+//             "Developed a website to showcase the findings and results of the project, using Wordpress and DIVI with CSS", 
+//             "Edited videos to showcase the project and its findings, using a combination of Blender 3D and Davinci Resolve", 
+//             "Improved protocols for DNA origmai structure and analysis using literature reviews and experimental data",
+//             "Developed posters and presentations to showcase the project and its findings", 
+//             "Implemented lab protocols for DNA origami characterization and purification analysis"
+//            : 
+//             ]
+//             results: {
+//               award: {
+//                 competition_1: "BIOMOD 2022 Japan Open",
+//               },
+//                    awards: [ " Audience Choice Award", 
+//                    "Best Presentation Award", 
+//                    "Best Project Website", 
+//                   "Gold Project Award", 
+//                  "Overall Winner"] 
+//                 }
+//              poster:[ 
+//                S. Nguyen, I. Abdi, M. Law., C. Sushams, E. Rajkumar, E. Halbe, M. Lei, L. Ju, R. Yeung,  "Novel and Stable Self Polymerization of DNA Origami Structures" (2020-2022)    
+//              ]
+//             }
+//           }
 
-//     }
+//           project_2: { 
+//             name: 
+//           }
+//         }
 
-//     { title
 
 //     }
 // ];
